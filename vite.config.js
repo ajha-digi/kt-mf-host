e98578592c1ed33fc146b00875868143d1d6c9a5
ajha-digi <ajha@digisprint.com>
2024-11-26 10:53:53 +0530

    implemeted mf app for host

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import federation from "@originjs/vite-plugin-federation";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: "demo-host-app",
+      filename: "remoteEntry.js",
+      remotes: {
+        demo_remote_app:"http://localhost:4040/assets/remoteEntry.js"
+      },
+      exposes: {},
+      shared: ["react", "react-dom"],
+    }),
+  ],
+
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
